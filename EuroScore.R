2622887451e5e6cd45357bb17a3a0d7b1bc6a044
raffdoc <raffdoc@yahoo.com>
2012-12-23 20:46:25 +0100

    Logistic score ajasted.

--- EuroScore.R
+++ EuroScore.R
@@ -122,7 +122,7 @@ EuroScoreLog.1  <- function(x,...) {
   if (!is.data.frame(x)) {
     stop("Data must be a dataframe", call. = FALSE)
   }
-    x$a.es <- NULL
+    x$log.es <- NULL
   # age variable scoring by age groups
   x$log.age <- NULL
   for(i in seq(along=x$age)) { 
@@ -142,81 +142,81 @@ EuroScoreLog.1  <- function(x,...) {
   # Extracardiac arteriopathy (One or more of claudication, carotid occlusion  or >50% stenosis, previous or planned intervention on the abdominal aorta, limb arteries or carotids)
   x$log.eca <- NULL
   for (i in seq(along=x$eca)) {
-    if(x$eca[i]==1){x$log.eca[i]<-1} else {x$log.eca[i]<- 0}
+    if(x$eca[i]==1){x$log.eca[i]<-0.6558917} else {x$log.eca[i]<- 0}
   }
   
   # Neurological disfunction (Disease severely affecting ambulation or day-to-day functioning)
   x$log.nd <- NULL
   for (i in seq(along=x$nd)) {
-    if(x$nd[i]==1){x$log.nd[i]<-1} else {x$log.nd[i]<- 0}
+    if(x$nd[i]==1){x$log.nd[i]<-0.6558917} else {x$log.nd[i]<- 0}
   }
   # Previous Cardiac Surgery
   x$log.pcs <- NULL
   for (i in seq(along=x$pcs)) {
-    if(x$pcs[i]==1){x$log.pcs[i]<-1} else {x$log.pcs[i]<- 0}
+    if(x$pcs[i]==1){x$log.pcs[i]<-1.002625} else {x$log.pcs[i]<- 0}
   }
   # Creatinin level > 200 µmol/ L
   x$log.creat <- NULL
   for (i in seq(along=x$creat)) {
-    if(x$creat[i]==1){x$log.creat[i]<-1} else {x$log.creat[i]<- 0}
+    if(x$creat[i]==1){x$log.creat[i]<-0.6521653} else {x$log.creat[i]<- 0}
   }
   # Active endocardities (Patient still on antibiotic treatment for endocarditis at time of surgery)
   x$log.ae <- NULL
   for (i in seq(along=x$ae)) {
-    if(x$ae[i]==1){x$log.ae[i]<-1} else {x$log.ae[i]<- 0}
+    if(x$ae[i]==1){x$log.ae[i]<-1.101265} else {x$log.ae[i]<- 0}
   }
   # Critical perioperative state (Ventricular Tachycardia / Ventricular Fibrillation or aborted sudden death, preoperative cardiac massage, preoperative ventilation before anaesthetic room, preoperative inotropes or IABP, preoperative Acute Renal Failure (anuria or oliguria <10ml/hr))
   x$log.cps <- NULL
   for (i in seq(along=x$cps)) {
-    if(x$cps[i]==1){x$log.cps[i]<-1} else {x$log.cps[i]<- 0}
+    if(x$cps[i]==1){x$log.cps[i]<-0.9058132} else {x$log.cps[i]<- 0}
   }
   # Anstable angina ( Rest angina requiring i.v. nitrates until arrival in anaesthetic room)
   x$log.ua <- NULL
   for (i in seq(along=x$ua)) {
-    if(x$ua[i]==1){x$log.ua[i]<-1} else {x$log.ua[i]<- 0}
+    if(x$ua[i]==1){x$log.ua[i]<-0.9058132} else {x$log.ua[i]<- 0}
   }
   # LV function espresed as EF
   x$log.lv.ef <- NULL
   for(i in seq(along=x$lv.ef)) {
     if (x$lv.ef[i]>50) { x$log.lv.ef[i] <- 0 } else { 
-      if (x$lv.ef[i]>30) { x$log.lv.ef[i] <- 1 } else { x$log.lv.ef[i] <- 3}
+      if (x$lv.ef[i]>30) { x$log.lv.ef[i] <- 0.4191643 } else { x$log.lv.ef[i] <- 1.094443}
     }}
   # Recent myocardial infarction (Myocardial infarction within 90 days)
   x$log.rmi <- NULL
   for (i in seq(along=x$rmi)) {
-    if(x$rmi[i]==1){x$log.rmi[i]<-1} else {x$log.rmi[i]<- 0}
+    if(x$rmi[i]==1){x$log.rmi[i]<-0.5460218} else {x$log.rmi[i]<- 0}
   }
   # Pulmonary hypertension (Systolic pulmonary artery pressure >60mmHg)
   x$log.ph <- NULL
   for (i in seq(along=x$ph)) {
-    if(x$ph[i]==1){x$log.ph[i]<-1} else {x$log.ph[i]<- 0}
+    if(x$ph[i]==1){x$log.ph[i]<-0.7676924} else {x$log.ph[i]<- 0}
   }
   # Emergency (Operation before beginning of next working day)
   x$log.em <- NULL
   for (i in seq(along=x$em)) {
-    if(x$em[i]==1){x$log.em[i]<-1} else {x$log.em[i]<- 0}
+    if(x$em[i]==1){x$log.em[i]<-0.7127953} else {x$log.em[i]<- 0}
   }
   # Other then isolated CABG 
   x$log.ot.icabg <- NULL
   for (i in seq(along=x$ot.icabg)) {
-    if(x$ot.icabg[i]==1){x$log.ot.icabg[i]<-1} else {x$log.ot.icabg[i]<- 0}
+    if(x$ot.icabg[i]==1){x$log.ot.icabg[i]<-0.5420364} else {x$log.ot.icabg[i]<- 0}
   }
   # Surgery on thoracic aorta
   x$log.sta <- NULL
   for (i in seq(along=x$sta)) {
-    if(x$sta[i]==1){x$log.sta[i]<-1} else {x$log.sta[i]<- 0}
+    if(x$sta[i]==1){x$log.sta[i]<-1.159787} else {x$log.sta[i]<- 0}
   }
   # Post infactual septal rupture
   x$log.pisr <- NULL
   for (i in seq(along=x$pisr)) {
-    if(x$pisr[i]==1){x$log.pisr[i]<-1} else {x$log.pisr[i]<- 0}
+    if(x$pisr[i]==1){x$log.pisr[i]<- 1.159787} else {x$log.pisr[i]<- 0}
   }
   # addative score output
   for (i in seq(along=x$log.age)){
-    x$a.es[i] <- exp{x$log.age[i]+x$log.sex[i]+x$log.cpd[i]+x$log.eca[i]+x$log.nd[i]+x$log.pcs[i]+ x$log.creat[i]+x$log.ae[i]+x$log.cps[i]+x$log.ua[i]+x$log.lv.ef[i]+x$log.rmi[i]+x$log.ph[i]+x$log.em[i]+x$log.ot.icabg[i]+x$log.sta[i]+x$log.pisr[i]
-  }}
-  #x<- data.frame(x,x$a.es)
-  x.out <- subset(x,select=c(age,sex,cpd,eca,nd,pcs,creat,ae,cps,ua,lv.ef,rmi,ph,em,ot.icabg,sta,pisr,a.es))
+    x$log.es[i] <- exp(x$log.age[i]+x$log.sex[i]+x$log.cpd[i]+x$log.eca[i]+x$log.nd[i]+x$log.pcs[i]+ x$log.creat[i]+x$log.ae[i]+x$log.cps[i]+x$log.ua[i]+x$log.lv.ef[i]+x$log.rmi[i]+x$log.ph[i]+x$log.em[i]+x$log.ot.icabg[i]+x$log.sta[i]+x$log.pisr[i]
+  )}
+  #x<- data.frame(x,x$log.es)
+  x.out <- subset(x,select=c(age,sex,cpd,eca,nd,pcs,creat,ae,cps,ua,lv.ef,rmi,ph,em,ot.icabg,sta,pisr,log.es))
   # return dataframe
   x.out <- data.frame(x.out)
   return(x.out)
